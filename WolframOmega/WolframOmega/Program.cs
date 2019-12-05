f727919776fa15131edf12a9dec579c0b1a906fa
CoralineRed <mustafinaanzhela@gmail.com>
2019-12-05 20:03:00 +0300

    meh~

--- WolframOmega/WolframOmega/Program.cs
+++ WolframOmega/WolframOmega/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot;
 
 namespace WolframOmega
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            var token = File.ReadAllLines("token.txt")[0];
+            var bot = new TelegramBotClient(token);
+            WriteAllUpdates(bot);
+            Console.ReadKey();
+        }
+
+        static async void WriteAllUpdates(TelegramBotClient bot)
+        {
+            await bot.SetWebhookAsync("");
+            var offset = 0;
+            while (true)
+            {
+                try
+                {
+                    var updates = await bot.GetUpdatesAsync(offset);
+                    foreach (var update in updates)
+                    {
+                        Console.WriteLine(update.Message);
+                        offset = update.Id + 1;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("не удалось соединиться с сервером");
+                }
+            }
         }
     }
 }
