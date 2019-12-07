44623ee1d5ee83861bd3c7cc8aed543f13fb30ce
CoralineRed <mustafinaanzhela@gmail.com>
2019-12-07 22:58:26 +0300

    [add] User, ICalculation

---
+++ WolframOmega/WolframOmega/ICalculation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WolframOmega
+{
+    interface ICalculation
+    {
+        string Name { get; }
+        string Message { get; }
+        string Calculate(string input);
+    }
+}
