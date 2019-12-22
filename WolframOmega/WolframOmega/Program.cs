a5e71e71bbf4cf16d489008330275376ee0b29e9
CoralineRed <mustafinaanzhela@gmail.com>
2019-12-23 00:13:43 +0300

    final

--- WolframOmega/WolframOmega/Program.cs
+++ WolframOmega/WolframOmega/Program.cs
@@ -13,49 +13,8 @@ namespace WolframOmega
     {
         static void Main(string[] args)
         {
-            var token = "866672360:AAHpvbtFgU4BK9EP7o_lsIfJTauLwhq3Pq8";
+            var token = File.ReadAllText("token.txt");
             new BotUi(token).Run();
-
-            //var bot = new TelegramBotClient(token);
-            //bot.OnMessage += (object sender, MessageEventArgs e) =>
-            //{
-            //    Console.WriteLine(e.Message.Text);
-            //    bot.SendTextMessageAsync(e.Message.Chat.Id, "Привет");
-            //};
-            //bot.StartReceiving();
-            //WriteAllUpdates(new TelegramBotClient(token));
-
-            //var a = new Arithmetic();
-            //var s = Console.ReadLine();
-            //while (s != "end")
-            //{
-            //    Console.WriteLine(a.Calculate(s));
-            //    s = Console.ReadLine();
-            //}
-
-            //Console.ReadKey();
-        }
-
-        static async void WriteAllUpdates(TelegramBotClient bot)
-        {
-            var offset = 0;
-            while (true)
-            {
-                try
-                {
-                    await bot.SetWebhookAsync("");
-                    var updates = await bot.GetUpdatesAsync(offset);
-                    foreach (var update in updates)
-                    {
-                        Console.WriteLine(update.Message);
-                        offset = update.Id + 1;
-                    }
-                }
-                catch
-                {
-                    Console.WriteLine("не удалось соединиться с сервером");
-                }
-            }
         }
     }
 }
